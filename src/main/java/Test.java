a5616db7a1db1754baba94578a4843e9dd373c54
lucaswen <lucaswen@tencent.com>
2023-11-23 17:53:31 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class Test {
-
+    // 等差数列
 	public static void main(String[] args) throws Exception{
 		
 		URL url = new URL("http://www.simuquanyi.com/index/voucher/index/id/16.html");
@@ -13,7 +13,11 @@ public class Test {
 		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 		
 		String s = "";
-		String s1 = "1123456";
+		String s1 = "666789";
+		String s2 = "123456";
+		String s3 = "123456";
+		String s4 = "123456";
+		String s5 = "123456";
 		System.out.println(br.readLine());
 		while((s=br.readLine()) != null){
 			System.out.println(s);
