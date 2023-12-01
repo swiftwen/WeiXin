3f889f1d05e576c0ed16cb610cede15f9d5a8991
lucaswen <lucaswen@tencent.com>
2023-12-01 18:30:27 +0800

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,104 +4,45 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * 
+ *
  * @author swiftwen
  * @date 2019年7月16日 下午2:11:29
  */
 public class Main {
-	final static int N = 210;
-	static int INF = 1000000000;
-	static char[][] g = new char[N][N];
+	//二叉树
+	//二叉树2
+    //二叉树3
+    //二叉树4
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10;
+        int k = 11;
+        int l = 12;
+        int m = 13;
+        int n = 14;
 
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int ca = in.nextInt();
-		while (ca-- > 0) {
-			int row = in.nextInt();
-			int col = in.nextInt();
-			if ((row & 1) == 0 && (col & 1) == 0 && (row>=4 || col>=4)) {
-                if(row>=col){
-                	for(int i=0;i<row;i++){
-                		for(int j=0;j<col;j++){
-                			if((j&1)==0){
-                				g[i][j] = '(';
-                			}else{
-                				g[i][j] = ')';
-                			}
-                		}
-                	}
-                	for(int i=0;i<row/2;i++){
-                		for(int j=0;j<col;j++){
-                			if(j<col/2){
-                				g[i][j] = '(';
-                			}else{
-                				g[i][j] = ')';
-                			}
-                		}
-                	}
-                	for(int i=0;i<row;i++){
-                    	for(int j=0;j<col;j++){
-                    		System.out.print(g[i][j]);
-                    	}
-                    	System.out.println();
-                    }
-                }else{
-                	for(int i=0;i<row;i++){
-                		for(int j=0;j<col;j++){
-                			if((i&1)==0){
-                				g[i][j] = '(';
-                			}else{
-                				g[i][j] = ')';
-                			}
-                		}
-                	}
-                	for(int j=0;j<col/2;j++){
-                		for(int i=0;i<row;i++){
-                			if(i<(row>>1)){
-                				g[i][j] = '(';
-                			}else{
-                				g[i][j] = ')';
-                			}
-                		}
-                	}
-                	for(int i=0;i<row;i++){
-                    	for(int j=0;j<col;j++){
-                    		System.out.print(g[i][j]);
-                    	}
-                    	System.out.println();
-                    }
-                }
-				continue;
-			}
-			if ((row & 1) == 0) {
-				for (int i = 0; i < row; i++) {
-					if ((i & 1) == 0) {
-						for (int j = 0; j < col; j++) {
-							System.out.print('(');
-						}
-					} else {
-						for (int j = 0; j < col; j++) {
-							System.out.print(')');
-						}
-					}
-					System.out.println();
-				}
-			} else {
-				for (int i = 0; i < row; i++) {
-					for (int j = 0; j < col; j++) {
-						if ((j & 1) == 0) {
-							System.out.print('(');
-						} else {
-							System.out.print(')');
-						}
-					}
-					System.out.println();
-				}
-			}
 
-		}
 
-		in.close();
-	}
+
+        
+    }
+    
+    
+    
+
+    
+    
+    
+    
+	
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,20 +5,16 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class Test {
-    // 等差数列
-	public static void main(String[] args) throws Exception{
+    // 快速排序
+
 		
 		URL url = new URL("http://www.simuquanyi.com/index/voucher/index/id/16.html");
 		URLConnection conn = url.openConnection();
 		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 		
 		String s = "";
-		String s1 = "666789";
-		String s2 = "123456";
-		String s3 = "123456";
-		String s4 = "123456";
-		String s5 = "123456";
-		System.out.println(br.readLine());
+		System.out.println(s);
+
 		while((s=br.readLine()) != null){
 			System.out.println(s);
 		}
