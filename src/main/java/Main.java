061e3eea4e996690364dec55b627175816f9d46e
lucaswen <lucaswen@tencent.com>
2023-12-12 14:46:10 +0800

    test1 [Reviewed by BK AIDev, transaction id: 337]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,13 +22,17 @@ public class Main {
         int e = 5;
         int f = 6;
         int g = 7;
-        int h = 8;
+        int h = 6;
         int i = 9;
         int j = 10;
         int k = 11;
         int l = 12;
         int m = 13;
         int n = 14;
+        int o = 15;
+        int p = 16;
+        int q = 17;
+
 
 
 
