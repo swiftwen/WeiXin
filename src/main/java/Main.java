c4d380e0aba51579820b00f9475da68221e3ac57
lucaswen <lucaswen@tencent.com>
2024-03-19 19:32:55 +0800

    m # Reviewed, transaction id: 4158

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,12 +22,6 @@ public class Main {
         int e = 5;
         int f = 6;
         int g = 7;
-        int h = 8;
-        int i = 9;
-        int j = 10;
-        int k = 11;
-        int l = 12;
-        int m = 13;
 
 
 
