3f11ba6e6c93c12338a34c957f79c4dada31f0af
lucaswen <lucaswen@tencent.com>
2024-03-29 15:43:49 +0800

    m # Reviewed, transaction id: 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,6 @@ import java.util.Scanner;
  * @date 2019年7月16日 下午2:11:29
  */
 public class Main {
-	//二叉树
-	//二叉树2
-    //二叉树3
-    //二叉树4
 
     public static void main(String[] args) {
         int a = 1;
