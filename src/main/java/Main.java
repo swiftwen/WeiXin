73d14261853c141c3bdbcd2b75e478c5b1fed2f0
lucaswen <lucaswen@tencent.com>
2024-04-01 14:44:00 +0800

    m # Reviewed, transaction id: 9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,13 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        
+        
+
 
 
     }
