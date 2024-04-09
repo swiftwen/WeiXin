53c51562d2f5f57728e8a3b68dcc582e7caa2336
swiftwen <wenpeng0412@qq.com>
2024-04-09 11:26:22 +0800

    tM # Reviewed, transaction id: 28

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,17 +14,7 @@ public class Main {
         int a = 1;
         int b = 2;
         int c = 3;
-        int d = 4;
-        int e = 5;
-        int f = 6;
-        int g = 7;
-        
 
-
-        
-
-        
-        
 
 
 
