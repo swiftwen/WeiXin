40442387a64fadcf720e25e469f128a3905e2876
swiftwen <wenpeng0412@qq.com>
2024-04-15 18:59:51 +0800

    t # Reviewed, transaction id: 16

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,6 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        int e = 5;
-        int f = 6;
-        int g = 7;
-        int h = 8;
 
         
         
