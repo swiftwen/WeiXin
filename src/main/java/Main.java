48218559f44178e4782cae9f1e18c311afec61a2
swiftwen <wenpeng0412@qq.com>
2024-04-26 10:19:12 +0800

    init # Reviewed, transaction id: 6814

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
-/**
- *
- * @author swiftwen
- * @date 2019年7月16日 下午2:11:29
- */
 public class Main {
-
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        
-
-
-        
-
-
-
-
-
-        
-
-        
-        
-
-
-        
-
+    public static void main(String[] args) throws Exception{
+        System.out.println("hello world");
     }
 
-
-    
-    
-
-    
-    
-    
-    
-	
 }
