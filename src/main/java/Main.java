596bbd5c2a3d4201cdc071be6c4600e246b3f2cc
swiftwen <wenpeng0412@qq.com>
2024-06-17 14:56:33 +0800

    t # Reviewed, transaction id: 9903

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,18 @@ public class Main {
         int b = 2;
         int c = a + b;
         System.out.println(c);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        
+
+        
+
+        
+
+
+
     }
 
 }
