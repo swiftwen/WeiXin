fd2c700ce5b1a5b9b1920c752e423a6853e1d2db
swiftwen <wenpeng0412@qq.com>
2024-07-08 15:53:00 +0800

    t

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,11 @@ public class Main {
         System.out.println("hello world");
         int a = 1;
         int b = 2;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("a + b = " + (a+b));
+
+        
         
     }
 
