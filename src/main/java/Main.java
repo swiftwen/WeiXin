3caf5336cde373d5cc77b6fb93e4cbe764a25622
swiftwen <wenpeng0412@qq.com>
2024-07-19 17:25:38 +0800

    t # Reviewed, transaction id: 12590

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,9 @@ public class Main {
         System.out.println("hello world");
         int a = 1;
         int b = 2;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("a + b = " + (a+b));
-        System.out.println("a -
+        int c = 3;
+        int d = 4;
+        int e = 5;
 
         
         
