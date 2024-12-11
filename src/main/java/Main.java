221876e302ec9155d66dc3a589dd196510da3a19
swiftwen <wenpeng0412@qq.com>
2024-12-11 14:48:58 +0800

    t1 # Reviewed, transaction id: 1923

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
         System.out.println("hello world");
         int a = 1;
         int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
     }
    
 
