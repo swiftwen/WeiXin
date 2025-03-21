7f7ecd3e0bb57672821f21a9c89cc444b85abf4f
swiftwen <wenpeng0412@qq.com>
2025-03-21 15:37:00 +0800

    t # Reviewed, transaction id: 35390

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         int a = 1;
+        int b = 2;
+        int c = 3;
+        
+        
 
 
 
